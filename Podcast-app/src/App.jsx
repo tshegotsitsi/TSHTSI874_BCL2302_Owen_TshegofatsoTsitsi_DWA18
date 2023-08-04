d9ffa80ae52901bad71daa04afb33b26617968a7
tshegotsitsi <125869406+tshegotsitsi@users.noreply.github.com>
2023-08-04 08:34:27 +0200

    Changes

--- Podcast-app/src/App.jsx
+++ Podcast-app/src/App.jsx
@@ -1,40 +1,95 @@
-import About from './Components/Pages/About';
-import Home from './Components/Pages/Home';
-import Podcasts from './Components/Pages/Podcasts';
-import ShowDetails from './Components/Pages/ShowDetails';
+import About from "./Components/Pages/About";
+import Home from "./Components/Pages/Home";
+import Podcasts from "./Components/Pages/Podcasts";
+import ShowDetails from "./Components/Pages/ShowDetails";
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-<BrowserRouter>
-
-
-<header className="bg-dark">
-
-<nav className="nav nav-pills flex-row P-2">
-  
-  <Link to="/" className="flex-sm-fill text-sm-center nav-link  text-light" aria-current="page" href="#">Home</Link>
-  <a className="flex-sm-fill text-sm-center nav-link dropdown-toggle text-light" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Genres</a>
-  <ul className="dropdown-menu">
-      <li><a className="dropdown-item" href="#">Personal Growth</a></li>
-      <li><a className="dropdown-item" href="#">True Crime& Investigative Journalism</a></li>
-      <li><a className="dropdown-item" href="#">History</a></li>
-      <li><a className="dropdown-item" href="#">Comedy</a></li>
-      <li><a className="dropdown-item" href="#">Entertainment</a></li>
-      <li><a className="dropdown-item" href="#">Business</a></li>
-      <li><a className="dropdown-item" href="#">Fiction</a></li>
-      <li><a className="dropdown-item" href="#">News</a></li>
-      <li><a className="dropdown-item" href="#">Kids & Family</a></li>
-    </ul>
-    <Link to="/podcasts" className="flex-sm-fill text-sm-center nav-link text-light" href="#">Podcasts</Link>
-  <Link to="/about" className="flex-sm-fill text-sm-center nav-link text-light" href="#">About</Link>
-  
-</nav>
-</header>
-
-
+    <BrowserRouter>
+      <header className="bg-dark">
+        <nav className="nav nav-pills flex-row P-2">
+          <Link
+            to="/"
+            className="flex-sm-fill text-sm-center nav-link  text-light"
+            aria-current="page"
+            href="#"
+          >
+            Home
+          </Link>
+          <a
+            className="flex-sm-fill text-sm-center nav-link dropdown-toggle text-light"
+            data-bs-toggle="dropdown"
+            href="#"
+            role="button"
+            aria-expanded="false"
+          >
+            Genres
+          </a>
+          <ul className="dropdown-menu">
+            <li>
+              <a className="dropdown-item" href="#">
+                Personal Growth
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                True Crime& Investigative Journalism
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                History
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Comedy
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Entertainment
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Business
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Fiction
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                News
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Kids & Family
+              </a>
+            </li>
+          </ul>
+          <Link
+            to="/podcasts"
+            className="flex-sm-fill text-sm-center nav-link text-light"
+            href="#"
+          >
+            Podcasts
+          </Link>
+          <Link
+            to="/about"
+            className="flex-sm-fill text-sm-center nav-link text-light"
+            href="#"
+          >
+            About
+          </Link>
+        </nav>
+      </header>
 
       <Routes>
         <Route path="/" element={<Home />} />
@@ -43,8 +98,7 @@ function App() {
         <Route path="/podcasts/:id" element={<ShowDetails />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
-
 
 export default App;
